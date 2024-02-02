bd3fd937d3bb0565c7014e3756a91e1ac5355287
Thamara Renoir <tha.renoir@gmail.com>
2024-02-02 17:04:42 +0100

    update

--- funcs1.R
+++ funcs1.R
@@ -3,6 +3,7 @@ extractMax <- function(data, nb_bloc, n, plot_bloc = TRUE, plot_max = TRUE,
                        bloc.title = paste(data, "par bloc"),
                        max.x = "x", max.y = "max[x]",
                        max.title = "Maximas") {
+  ## Extraction des maximas par bloc
   
   maxX <- rep(0, nb_bloc)
   ind <- rep(0, nb_bloc)
@@ -23,10 +24,11 @@ extractMax <- function(data, nb_bloc, n, plot_bloc = TRUE, plot_max = TRUE,
 
 IC_GEV <- function(data, alpha) {
   ## Renvoie les intervalles de confiance des paramètres d'un objet gev.fit
-  ## calculés par maximum de vraisemblance
+  ## Calculés par maximum de vraisemblance
   
   # data : objet gev.fit
   # alpha : niveau de contrôle
+  
   IC <- cbind(data$mle - qnorm(p = 1 - alpha/2) * data$se,
               data$mle + qnorm(p = 1 - alpha/2) * data$se)
   rownames(IC) <- c("mu", "sigma", "gamma")
@@ -35,15 +37,24 @@ IC_GEV <- function(data, alpha) {
   return(IC)
 }
 
-# Calcul les valeurs de retour et leur intervalle de confiance
 quant <- function(gev, q, mu, sigma, gama, alpha) {
+  ## Calcul des niveaux de retour et leur intervalle de confiance
+  
+  # gev : objet gev.fit
+  # q : probabilité qu'un max dépasse le niveau de retour
+  #     associé à la période de retour 1/q
+  # mu : paramètre de moyenne (location)
+  # sigma : paramètre de dispersion
+  # gama : paramètre de forme
+  # alpha : niveaux de l'intervalle de confiance
+  
   yq <- -log(1-q)
   xq <- mu - (sigma / gama) * (1 - yq^(-gama)) 
   
   gradxq <- c(1, - gama^(-1) * (1 - yq^(-gama)),
               sigma * gama^(-2) * (1 - yq^(-gama)) - (sigma / gama) * 
                 yq^(-gama) * log(yq))
-  varxq <- t(gradxq1) %*% gev$cov %*% gradxq
+  varxq <- t(gradxq) %*% gev$cov %*% gradxq
   IC <- c(xq - qnorm(1 - alpha/2) * sqrt(varxq),
           xq + qnorm(1 - alpha/2) * sqrt(varxq))
   
--- script1.R
+++ script1.R
@@ -13,27 +13,35 @@ source("funcs1.R")
 str(buoysInfos)
 str(donneesVague)
 
-SA <- donneesVague$station1
+SA <- donneesVague$station6
 head(SA)
 m <- length(SA)
 
+SB <- donneesVague$station14
 
-############################### AJUSTELEMENT GEV ############################### 
+############################### AJUSTELEMENT GEV ###############################
 
+#### Extraction des maximas ####
 
-#### maximas ####
+df <- data.frame(date = donneesVague$date, station = SA)
 
-# par an
-df <- data.frame(date = donneesVague$date, station = donneesVague$station1)
-
+# Extraction de l'année
 df$year <- format(as.Date(df$date, format="%Y-%m-%d"),"%Y")
-df$month <- format(as.Date(df$date, format = "%Y-%m-%d"), "%d")
+# Extraction du mois
+df$month <- format(as.Date(df$date, format = "%Y-%m-%d"), "%m")
 
+# Max par an
 maxX1 <- df %>% group_by(year) %>% summarise(Max = max(station)) %>% pull()
+length(maxX1)
+# Nombre de mesures par an
 eff1 <- df %>% group_by(year) %>% count() %>% pull()
 
+# Max par mois
 maxX2 <- df %>% group_by(year, month, .add = TRUE) %>%
   summarise(Max = max(station)) %>% pull()
+length(maxX2)
+# Nombre de mesures par mois
+eff2 <- df %>% group_by(year, month, .add = TRUE) %>% count() %>% pull()
 
 # # par bloc
 # nb_bloc <- m / 1060 # 438 blocs (maximas)
@@ -43,7 +51,7 @@ maxX2 <- df %>% group_by(year, month, .add = TRUE) %>%
 #                    max.x = "Indice", max.y = "Hauteur",
 #                    max.title = "Hauteurs de vagues maximales")
 
-#### ajustement ####
+#### Ajustement ####
 
 # ajout de tendence à discuter
 
@@ -51,29 +59,37 @@ maxX1_GEV1 <- gev.fit(maxX1)
 maxX1_GEV1$mle
 maxX1_GEV1$nllh
 
+# Paramètres
 mu1 <- maxX1_GEV1$mle[1]
 sigma1 <- maxX1_GEV1$mle[2]
 gamma1 <- maxX1_GEV1$mle[3] # gamma < 0 => Weibull
 
 gev.diag(maxX1_GEV1) # Correct
 
+# Intervalle de confiance
 IC_GEV(maxX1_GEV1, alpha = 0.05) # IC_gamma contient 0, tester si Gumbel mieux
 
-# ajustement d'une Gumbel
+# Ajustement d'une Gumbel
 maxX1_gum <- gum.fit(maxX1)
-gum.diag(maxX1_gum)
+gum.diag(maxX1_gum) # Retrun level et Quantile plot moins convaincants
 
-# test gamma = 0
+# Comparaison des modèles : Test gamma = 0
 2 * (maxX1_GEV1$nllh - maxX1_gum$nllh) <= qchisq(0.95, 1)
 # On rejette gamma = 0
 
-#### niveaux de retour ####
+#### Niveaux de retour ####
 
-# Quantiles
+# Périodes et probas
 T1 <- 100 ; q1 <- 1/T1 ; yq1 <- - log(1 - q1)
 T2 <- 500 ; q2 <- 1/T2 ; yq2 <- - log(1 - q2)
 T3 <- 1000 ; q3 <- 1/T3 ; yq3 <- - log(1 - q3)
 
+# Quantiles
 quant(maxX1_GEV1, 1/100, mu1, sigma1, gamma1, 0.05)
 quant(maxX1_GEV1, 1/500, mu1, sigma1, gamma1, 0.05)
 quant(maxX1_GEV1, 1/1000, mu1, sigma1, gamma1, 0.05)
+
+############################### AJUSTELEMENT GPD ###############################
+
+# Choix du seuil
+mrlplot(SA)  # trop lourd à faire tourner, alternative nécessaire ?
